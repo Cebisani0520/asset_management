4a57a5bdc19362fe74a1138bdab61dcd8d7fe189
Cebisani0520 <cebisani0520>
2025-04-06 13:01:12 +0200

    add common library package

---
+++ backend/common-lib/src/main/java/za/co/common/events/DepartmentEvent.java
@@ -0,0 +1,27 @@
+package za.co.common.events;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import za.co.common.enums.EventTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class DepartmentEvent extends BaseEvent {
+    private Long departmentId;
+    private String name;
+    private String description;
+    private EventTypes departmentEventType;
+
+    public DepartmentEvent(EventTypes type, Long departmentId, String name, String description) {
+        super(UUID.randomUUID(), "DEPARTMENT_EVENT", LocalDateTime.now());
+        this.departmentEventType = type;
+        this.departmentId = departmentId;
+        this.name = name;
+        this.description = description;
+    }
+}
