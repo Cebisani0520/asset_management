74c67fa67a3c8f7adb59b8a65ec8adcd2c357802
Cebisani0520 <cebisani0520>
2025-04-30 15:22:21 +0200

    update kafka config file.
    This updates the topic creation bean configuration.

--- backend/department-service/src/main/java/za/co/department/config/KafkaConfig.java
+++ backend/department-service/src/main/java/za/co/department/config/KafkaConfig.java
@@ -4,15 +4,15 @@ package za.co.department.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+@Configuration
 public class KafkaConfig {
-    @Value("${kafka.topic.department-events}")
-    private String departmentEventsTopic;
 
     @Bean
-    public NewTopic departmentEventsTopic() {
-        return TopicBuilder.name(departmentEventsTopic)
+    public NewTopic departmentEventsTopic(final KafkaTopicConfigProps kafkaTopicConfigProps) {
+        return TopicBuilder.name(kafkaTopicConfigProps.getDepartmentEvents())
                 .partitions(3)
                 .replicas(1)
                 .build();
